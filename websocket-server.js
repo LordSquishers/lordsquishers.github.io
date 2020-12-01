cfad6ccf5adaa0f7f15c7cd4f4b5f66e3d9e988d
LordSquishers <lordsquisherton@gmail.com>
2020-12-01 18:08:18 +0000

    wrong message name

--- websocket-server.js
+++ websocket-server.js
@@ -49,7 +49,7 @@ wsServer.on('request', function(request) {
 
     connection.on('message', function(message) {
       console.log('Received Message:', message.utf8Data);
-      if(message.utf8Data.startsWith('CHARCOAL:')) {
+      if(message.utf8Data.startsWith('CHARCOAL_PERCENT:')) {
         var splits = message.utf8Data.split(':');
         // call construction.js function to update index
         console.log('Received charcoal data from base: ' + splits[1]);
