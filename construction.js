2078f4c010057ba8c3d2f90c12490223b48211b7
LordSquishers <lordsquisherton@gmail.com>
2020-12-01 18:26:49 +0000

    p

--- construction.js
+++ construction.js
@@ -1,10 +1,2 @@
 // pog
 console.log('Construction initialized!');
-const fs = require('fs');
-
-fs.readFile('char.txt', function (err, data) {
-   if (err) {
-      return console.error(err);
-   }
-   document.getElementById('charDisplay').innerHTML = data.toString();
-});
