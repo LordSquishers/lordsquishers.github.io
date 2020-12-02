67745d431309916c0c70dfa538f18824ac45ac35
LordSquishers <lordsquisherton@gmail.com>
2020-12-02 11:57:15 +0000

    added timestamp to log

--- websocket-server.js
+++ websocket-server.js
@@ -88,6 +88,14 @@ wsServer.on('request', function(request) {
 
         if(charHistStream == null) {
           charHistStream = fs.createWriteStream("charHistory.txt", {flags:'a'});
+          var currentdate = new Date();
+          charHistStream.write(
+                var datetime = "Last Sync (CC ping 20 sec): " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/"
+                + currentdate.getFullYear() + " @ "
+                + currentdate.getHours() + ":"
+                + currentdate.getMinutes() + ":"
+                + currentdate.getSeconds() + "\n\n");
         }
         charHistStream.write(percentage + "\n");
 
